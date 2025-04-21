e5b4cbb881710a02b20e4a97957605706ed818b8
DareAnanas <nylo2005@gmail.com>
2025-04-21 21:32:22 +0300

    made on_path_change

--- fileManager/main.py
+++ fileManager/main.py
@@ -2,9 +2,17 @@ from kivy.app import App
 from kivy.uix.relativelayout import RelativeLayout
 from contextmenu import AppMenu, AppMenuTextItem, \
 ContextMenu, ContextMenuTextItem
+from kivy.properties import ObjectProperty
 
 class FileManager(RelativeLayout):
-    pass
+    fileView = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.fileView.bind(path=self.on_path_change)
+
+    def on_path_change(self, fileView, path):
+        print(path)
 
 class FileManagerApp(App):
     def build(self):
