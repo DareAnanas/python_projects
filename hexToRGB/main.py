7d8b8da78cda5cdf52e0a1c3abbcf42a4059b4e5
DareAnanas <nylo2005@gmail.com>
2025-05-12 11:56:40 +0300

    changed rect color to brown

--- hexToRGB/main.py
+++ hexToRGB/main.py
@@ -6,7 +6,7 @@ def hexToRgba(hex_color, alpha=1):
     return [round(r / 255.0, 4), round(g / 255.0, 4), round(b / 255.0, 4), alpha]
 
 
-print(hexToRgba('#ff5500'))
+print(hexToRgba('#A52A2A'))
 
 # print(int(0.11764705882352941 * 255))
 
